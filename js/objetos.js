d4681db9fa8a98aefb07c8200813d09f207c5335
Fernando Cuellar <fercuellar@gmail.com>
2023-03-11 11:51:38 -0600

    desestructuracion

--- js/objetos.js
+++ js/objetos.js
@@ -1,30 +1,102 @@
-const persona = {
-    nombre: 'Fernando',
-    apellido: 'Cuellar',
-    edad: 47,
-    estaActivo: true,
-};
+// const persona = {
+//     nombre: 'Fernando',
+//     apellido: 'Cuellar',
+//     edad: 47,
+//     estaActivo: true,
+// };
 
+// // console.log(persona);
+
+// // Imprimir propiedades
+// console.log(persona.nombre);
+// console.log(persona.apellido);
+// console.log(persona['apellido']);
+
+// console.log(persona.carrera);
+
+// // Cambiar un valor
+// persona.edad = 47;
+// persona.nombre = 'Juan';
 // console.log(persona);
 
-// Imprimir propiedades
-console.log(persona.nombre);
-console.log(persona.apellido);
-console.log(persona['apellido']);
+// // Agregar atributo o propiedad
+// persona.profesion = 'Ingeniero';
+// console.log(persona.profesion);
 
-console.log(persona.carrera);
+// // Eliminar atributos
+// console.log(persona)
+// delete persona.nombre;
+// console.log(persona)
 
-// Cambiar un valor
-persona.edad = 47;
-persona.nombre = 'Juan';
-console.log(persona);
+// ejercicio 1
 
-// Agregar atributo o propiedad
-persona.profesion = 'Ingeniero';
-console.log(persona.profesion);
+// // Crear el objeto
+// const personalInfo = {
+//     nombre: "Juan",
+//     edad: 27,
+//     ocupacion: "Desarrollador"
+// };
 
-// Eliminar atributos
-console.log(persona)
-delete persona.nombre;
-console.log(persona)
+// // Metodo para actualizar propiedades del objeto
+// function actualizarPersonalInfo(propiedad, valor) {
+//     personalInfo[propiedad] = valor;
+// };
+
+// actualizarPersonalInfo("ocupacion", "Diseñador");
+// console.log(personalInfo);
+
+
+// Iteracion de objetos for in
+
+// const persona = {
+//     name: 'Fernando',
+//     age: 47,
+//     occupacion: 'Developer'
+// };
+
+// for (let prop in persona) {
+//     console.log(`${prop}: ${persona[prop]}`);
+// };
+
+// // Iteracion de objetos recorrer un objeto
+// // Object.keys
+
+// const persona = {
+//     name: 'Fernando',
+//     age: 47,
+//     occupacion: 'Developer',
+// };
+
+// const prop = Object.keys(persona);
+
+//   // Iterando sobre las claves con forEach()
+// prop.forEach(key => {
+//     console.log(`${key}: ${persona[key]}`);
+// });
+
+
+// Desestructuracion de objetos
+
+// const persona = {
+//     nombre: 'Fernando',
+//     apellido: 'Cuellar',
+//     edad: 47,
+// };
+
+// const {nombre, apellido, edad} = persona;
+
+//   console.log(nombre); // valor1
+//   console.log(apellido); // valor2
+//   console.log(edad); // valor3
+
+
+// const objeto1 = {
+//     propiedad1: 'valor1',
+//     propiedad21: 'valor2'
+// };
+
+// const {propiedad1: nuevaPropiedad, propiedad21 = 'valor predeterminado'} = objeto;
+
+//   console.log(nuevaPropiedad); // valor1
+//   console.log(propiedad2); // valor2 (del objeto original)
 

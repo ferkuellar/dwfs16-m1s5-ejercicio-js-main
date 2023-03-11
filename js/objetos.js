56b58c57d85beb2645bbb6cfd762329f1c93bdbd
Fernando Cuellar <fercuellar@gmail.com>
2023-03-11 13:01:25 -0600

    Ejercicio 3

--- js/objetos.js
+++ js/objetos.js
@@ -115,30 +115,100 @@
 // La desestructuración es una característica incorporada en ES6 que nos permite extraer valores de arrays o propiedades de objetos y asignarlas a variables separadas.
 // En la desestructuración de arreglos podemos extraer los valores almacenados en ciertos índices del arreglo y asignarlos a variables separadas de manera fácil y rápida.
 
-const miArreglo = [1,2,3];
+// const miArreglo = [1,2,3];
 
-const [a,b,c] = miArreglo; // Esto tendrá como resultado  a=1, b=2, c=3.
+// const [a,b,c] = miArreglo; // Esto tendrá como resultado  a=1, b=2, c=3.
 
-console.log(a); // 1
-console.log(b); // 2
-console.log(c); // 3 
+// console.log(a); // 1
+// console.log(b); // 2
+// console.log(c); // 3 
 
 // Es importante mencionar que podemos omitir uno o más elementos de un arreglo. Es tan simple como dejar un espacio en blanco dentro del [] donde iba el elemento que queremos omitir.
 
-const miArreglo1 = [1,2,3];
+// const miArreglo1 = [1,2,3];
 
-const [d, ,e] = miArreglo; // Esto tendrá como resultado  d=1, e=3.
+// const [d, ,e] = miArreglo; // Esto tendrá como resultado  d=1, e=3.
 
-console.log(d); // 1
-console.log(e); // 3 
+// console.log(d); // 1
+// console.log(e); // 3 
 
 // También es posible hacer una asignación por defecto a las variables, en caso de que no se encuentre ese valor en el arreglo:
 
-const miArreglo3 = [1];
+// const miArreglo3 = [1];
 
-const [f=0, g=2, h=3] = miArreglo3;  // aquí, el valor c no se encuentra en el arreglo, por lo que se le asigna un valor por defecto de 3.
+// const [f=0, g=2, h=3] = miArreglo3;  // aquí, el valor c no se encuentra en el arreglo, por lo que se le asigna un valor por defecto de 3.
 
-console.log(f); // 1
-console.log(g); // 2
-console.log(h); // 3 
+// console.log(f); // 1
+// console.log(g); // 2
+// console.log(h); // 3 
 
+// EJERCICO 2
+
+// Crear un metodo en javascript que permita agregar un nuevo valor a un objeto global y si ya existe esa propiedad, imprima un mensaje que ya existe la propiedad. Si no, que agregue la propiedad.
+
+// Para crear un método en JavaScript que permita agregar valores a un objeto global y verifique si ya existe la propiedad, se puede utilizar el siguiente código:
+
+// const informacionPersonal = {
+//     nombre: "Fernando",
+//     edad: 47,
+//     ocupacion: "Desarrollador"
+// };
+
+// function agregarPropiedad(propiedad, valor) {
+//     // Verificar si la propiedad ya existe
+//     if (informacionPersonal.hasOwnProperty(propiedad)) {
+//     console.log(`La propiedad "${propiedad}" ya existe en el objeto.`);
+//     } else {
+//     informacionPersonal[propiedad] = valor;
+//     console.log(`Se ha agregado la propiedad "${propiedad}" al objeto.`);
+//     }
+// }
+
+//   // Ejemplo de uso
+//   agregarPropiedad("nombre", "Fernando"); // La propiedad "nombre" ya existe en el objeto.
+//   agregarPropiedad("email", "fernando@correo.com"); // Se ha agregado la propiedad "email" al objeto.
+
+//   console.log(informacionPersonal); // Imprimir objeto completo
+
+//   En este ejemplo, primero creamos un objeto llamado informacionPersonal con algunas propiedades. Luego, creamos una función llamada agregarPropiedad que toma dos parámetros, propiedad y valor. La función verifica si la propiedad ya existe en el objeto utilizando el método hasOwnProperty(), que devuelve true si la propiedad existe y false si no existe. Si la propiedad ya existe, se imprime un mensaje indicando eso. Si la propiedad no existe, se agrega al objeto utilizando la sintaxis de corchetes ([]) y se imprime un mensaje indicando eso. Al final, se imprime el objeto completo para verificar los cambios realizados.
+
+
+// EJERCICIO 3
+
+// Crear una variable en javascript que sea un array y crear dos funciones: 1.- el primero debe agregar al array la informacion de un libro en forma de objeto. 
+// 2.- el segundo debe imprimir el arreglo aobjetos. 
+// De tal forma que se puedan mandar a llamar la consola las funciones para agrgar mas informacion al arreglo o para imprimir todos los elementos del arreglo.
+
+// Primero, crearemos una variable books que será un array vacío
+
+let books = [];
+
+// Luego, crearemos la primera función para agregar información de libros al arreglo. Tomará como parámetros el título, autor y género del libro.
+
+function agregarLibro(titulo, autor, genero) {
+    let libro = {
+    titulo: titulo,
+    autor: autor,
+    genero: genero
+    };
+    books.push(libro);
+}
+
+// Por último, crearemos la segunda función para imprimir los elementos del arreglo. Iterará sobre el arreglo y mostrará en consola el título, autor y género de cada libro.
+
+function imprimirLibros() {
+    for (let i = 0; i < books.length; i++) {
+    console.log(`Título: ${books[i].titulo}, Autor: ${books[i].autor}, Género: ${books[i].genero}`);
+    }
+}
+
+// Ahora, podemos llamar a ambas funciones para agregar información de libros al arreglo y posteriormente imprimirlos en consola.
+
+//Agregar información de libros al arreglo
+agregarLibro("La Sombra del Viento", "Carlos Ruiz Zafón", "Misterio");
+agregarLibro("Harry Potter y la Piedra Filosofal", "J.K. Rowling", "Fantasía");
+
+//Imprimir elementos del arreglo
+imprimirLibros();
+
+// Si quisieramos agregar más libros al arreglo, simplemente llamaríamos a la primera función con el nuevo título, autor y género.
